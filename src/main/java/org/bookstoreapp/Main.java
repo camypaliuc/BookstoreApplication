99ee00ae5f4b296ec74859aad78c148b617173a6
Camelia Paliuc <camy_paliuc@yahoo.com>
2020-04-28 15:12:27 +0300

    BA-2/ Initial commit: Project SetUp and Main Class.

---
+++ src/main/java/org/bookstoreapp/Main.java
@@ -0,0 +1,16 @@
+package org.bookstoreapp;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String args[]){
+       launch(args);
+    }
+    @Override
+    public void start(Stage primarystage) throws Exception{
+
+    }
+}
