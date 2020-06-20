72f2d81a864309d854ed892cf140dc877ac332be
Floroiu Andrei <floandrei21@gmail.com>
2020-06-20 16:56:57 +0300

    BA-11\Created a GUI for edit price. Added the EditPriceController and EditPrice button.

--- src/main/java/org/bookstoreapp/controllers/BookController.java
+++ src/main/java/org/bookstoreapp/controllers/BookController.java
@@ -1,13 +1,22 @@
 package org.bookstoreapp.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import org.bookstoreapp.model.Book;
+import org.bookstoreapp.model.User;
 import org.bookstoreapp.services.CartService;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 
@@ -49,5 +58,19 @@ public class BookController {
     public void handleAddToCartAction() {
         CartService.AddBooktoCart(book);
 
+    }
+
+    public static User thisuser = RegistrationController.currentuser;
+    @FXML
+
+    public void handleEditPriceButton(ActionEvent event) throws IOException {
+        if (((String) thisuser.getRole()) == "Admin"){
+            Parent home_page_parent = FXMLLoader.load(getClass().getClassLoader().getResource("EditPrice.fxml"));
+            Scene home_page_scene = new Scene(home_page_parent);
+            Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            app_stage.hide();
+            app_stage.setScene(home_page_scene);
+            app_stage.show();
+        }
     }
 }
